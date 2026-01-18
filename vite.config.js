5efbc050a0a070f86bcb26d138a552f7a7910f1e
DrouetDanilo <ddrouet@espol.edu.ec>
2026-01-18 01:13:57 +0000

    arreglando bugs

--- vite.config.js
+++ vite.config.js
@@ -26,8 +26,10 @@ export default defineConfig({
             type: 'image/png'
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB
       }
     })
   ]
 })
-
